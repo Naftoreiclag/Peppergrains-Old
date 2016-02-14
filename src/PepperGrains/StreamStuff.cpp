dffdc5234c9c349664e19bd24059a0f552d85d73
naftoreiclag <naftoreiclag@gmail.com>
2016-02-14 10:11:31 -0800

    Improve reading functions

--- src/PepperGrains/StreamStuff.cpp
+++ src/PepperGrains/StreamStuff.cpp
@@ -2,44 +2,65 @@
 
 namespace pgg {
 
-uint32_t readU32(std::ifstream& input) {
-    uint32_t value;
-    input.read((char*) &value, sizeof value);
-    return value;
+void readBool(std::ifstream& input, bool& value) {
+    char byte;
+    input.read(&byte, sizeof byte);
+    value = byte != 0;
 }
-
 bool readBool(std::ifstream& input) {
     char value;
     input.read(&value, sizeof value);
     return value != 0;
 }
 
+void readU32(std::ifstream& input, uint32_t& value) {
+    input.read((char*) &value, sizeof value);
+}
+uint32_t readU32(std::ifstream& input) {
+    uint32_t value;
+    readU32(input, value);
+    return value;
+}
+
+void readU16(std::ifstream& input, uint16_t& value) {
+    input.read((char*) &value, sizeof value);
+}
 uint16_t readU16(std::ifstream& input) {
     uint16_t value;
+    readU16(input, value);
+    return value;
+}
+
+void readU8(std::ifstream& input, uint8_t& value) {
     input.read((char*) &value, sizeof value);
-    return value;
 }
-
 uint8_t readU8(std::ifstream& input) {
-    char value;
-    input.read(&value, sizeof value);
+    uint8_t value;
+    readU8(input, value);
     return value;
 }
 
+void readF32(std::ifstream& input, float& value) {
+    input.read((char*) &value, sizeof value);
+}
 float readF32(std::ifstream& input) {
     float value;
-    input.read((char*) &value, sizeof value);
+    readF32(input, value);
     return value;
 }
 
-std::string readString(std::ifstream& input) {
+void readString(std::ifstream& input, std::string& value) {
     uint32_t size = readU32(input);
     char* buff = new char[size + 1];
     input.read(buff, size);
     buff[size] = '\0';
-    std::string r = buff;
+    value = buff;
     delete[] buff;
-    return r;
+}
+std::string readString(std::ifstream& input) {
+    std::string value;
+    readString(input, value);
+    return value;
 }
 
 }
--- src/PepperGrains/StreamStuff.hpp
+++ src/PepperGrains/StreamStuff.hpp
@@ -7,16 +7,22 @@
 
 namespace pgg {
 
-uint32_t readU32(std::ifstream& input);
-
+void readBool(std::ifstream& input, bool& value);
 bool readBool(std::ifstream& input);
 
+void readU32(std::ifstream& input, uint32_t& value);
+uint32_t readU32(std::ifstream& input);
+
+void readU16(std::ifstream& input, uint16_t& value);
 uint16_t readU16(std::ifstream& input);
 
+void readU8(std::ifstream& input, uint8_t& value);
 uint8_t readU8(std::ifstream& input);
 
+void readF32(std::ifstream& input, float& value);
 float readF32(std::ifstream& input);
 
+void readString(std::ifstream& input, std::string& value);
 std::string readString(std::ifstream& input);
 
 }
