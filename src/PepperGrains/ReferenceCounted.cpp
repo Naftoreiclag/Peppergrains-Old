6ce90c71af4b3818bd664d24ed230e53aa2a4d5b
naftoreiclag <naftoreiclag@gmail.com>
2016-02-14 23:15:10 -0800

    Encapsulate reference counting

--- src/PepperGrains/ReferenceCounted.cpp
+++ src/PepperGrains/ReferenceCounted.cpp
@@ -11,33 +11,32 @@
    limitations under the License.
 */
 
-#include "Resource.hpp"
+#include "ReferenceCounted.hpp"
 
 #include <cassert>
 
 namespace pgg {
 
-Resource::Resource() { }
-Resource::~Resource() { }
+ReferenceCounted::ReferenceCounted()
+: mNumGrabs(0) { }
+ReferenceCounted::~ReferenceCounted() { }
 
-void Resource::setFile(const boost::filesystem::path& file) {
-    assert(mFile == boost::filesystem::path() && "File loading path is set twice.");
-    mFile = file;
+void ReferenceCounted::grab() {
+    ++ mNumGrabs;
+
+    if(mNumGrabs > 0) {
+        this->load();
+    }
 }
-const boost::filesystem::path& Resource::getFile() {
-    return mFile;
+
+void ReferenceCounted::drop() {
+    assert(mNumGrabs != 0);
+    -- mNumGrabs;
+
+    if(mNumGrabs == 0) {
+        this->unload();
+    }
 }
-void Resource::setName(std::string name) {
-    mName = name;
-}
-const std::string& Resource::getName() {
-    return mName;
-}
-void Resource::setSize(uint32_t size) {
-    mFileSize = size;
-}
-const uint32_t& Resource::getSize() {
-    return mFileSize;
-}
+
 
 }
--- src/PepperGrains/ReferenceCounted.hpp
+++ src/PepperGrains/ReferenceCounted.hpp
@@ -11,34 +11,27 @@
    limitations under the License.
 */
 
-#ifndef PGG_RESOURCE_HPP
-#define PGG_RESOURCE_HPP
+#ifndef PGG_REFERENCECOUNTED_HPP
+#define PGG_REFERENCECOUNTED_HPP
 
 #include <stdint.h>
 
-#include <boost/filesystem.hpp>
-
-#include "ReferenceCounted.hpp"
-
 namespace pgg {
 
-class Resource : virtual public ReferenceCounted {
+class ReferenceCounted {
 private:
-    uint32_t mFileSize;
-    std::string mName;
-    boost::filesystem::path mFile;
+    uint32_t mNumGrabs;
 public:
-    Resource();
-    virtual ~Resource();
-    
-    void setFile(const boost::filesystem::path& file);
-    const boost::filesystem::path& getFile();
-    void setName(std::string name);
-    const std::string& getName();
-    void setSize(uint32_t size);
-    const uint32_t& getSize();
+    ReferenceCounted();
+    virtual ~ReferenceCounted();
+
+    void grab();
+    void drop();
+
+    virtual bool load() = 0;
+    virtual bool unload() = 0;
 };
 
 }
 
-#endif // RESOURCE_HPP
+#endif // PGG_REFERENCECOUNTED_HPP
