d373b3e0978972a165fb9615097ea7856e36529f
Naftoreiclag <naftoreiclag@gmail.com>
2016-06-09 23:46:09 -0700

    File fission

---
+++ src/PepperGrains/DeferredRenderer.hpp
@@ -0,0 +1,138 @@
+/*
+   Copyright 2016 James Fong
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef PGG_DEFERREDRENDERER_HPP
+#define PGG_DEFERREDRENDERER_HPP
+
+#include <stdint.h>
+
+#include "OpenGLStuff.hpp"
+#include "HardValueStuff.hpp"
+#include "ReferenceCounted.hpp"
+#include "SceneNode.hpp"
+#include "ShaderProgramResource.hpp"
+#include "SunLightModel.hpp"
+
+namespace pgg {
+
+class DeferredRenderer : public ReferenceCounted {
+public:
+    class SharedResources : public ReferenceCounted {
+    public:
+    
+        GLuint mFullscreenVao;
+        GLuint mFullscreenVbo;
+        GLuint mFullscreenIbo;
+    
+        struct GBuffer {
+            GLuint gFramebuffer;
+            
+            GLuint diffuseTexture;
+            GLuint normalTexture;
+            GLuint brightTexture;
+            GLuint depthStencilTexture;
+        };
+        GBuffer mGBuff;
+
+        struct ScreenShader {
+            ShaderProgramResource* shaderProg;
+            
+            GLuint diffuseHandle;
+            GLuint brightHandle;
+        };
+        ScreenShader mScreenShader;
+        
+        struct DebugScreenShader {
+            ShaderProgramResource* shaderProg;
+            
+            GLuint diffuseHandle;
+            GLuint normalHandle;
+            GLuint depthHandle;
+            GLuint brightHandle;
+            
+            GLuint viewHandle;
+        };
+        DebugScreenShader mDebugScreenShader;
+        
+        struct SkyStencilShader {
+            ShaderProgramResource* shaderProg;
+        };
+        SkyStencilShader mSkyStencilShader;
+        
+        struct DebugScreenFillShader {
+            ShaderProgramResource* shaderProg;
+            GLuint colorHandle;
+        };
+        DebugScreenFillShader mFillScreenShader;
+        
+        struct Camera {
+            float fov;
+            float aspect;
+            float nearDepth;
+            float farDepth;
+            
+            float cascadeBorders[PGG_NUM_SUN_CASCADES + 1];
+            
+            glm::mat4 viewMat;
+            glm::mat4 projMat;
+            glm::vec3 position;
+        };
+        Camera mCamera;
+        
+        struct Sky {
+            uint32_t sunTextureSize;
+            glm::vec3 sunDirection;
+            glm::vec3 sunPosition;
+            
+            GLuint sunFramebuffers[PGG_NUM_SUN_CASCADES];
+            GLuint sunDepthTextures[PGG_NUM_SUN_CASCADES];
+            
+            glm::mat4 sunProjMatrices[PGG_NUM_SUN_CASCADES];
+            glm::mat4 sunViewMatrices[PGG_NUM_SUN_CASCADES];
+            
+            glm::mat4 sunBasicViewMatrix;
+            
+            SunLightModel* sunModel;
+        };
+        Sky mSky;
+        
+        SharedResources();
+        ~SharedResources();
+    public:
+        static SharedResources* getSingleton();
+        uint32_t mScreenWidth;
+        uint32_t mScreenHeight;
+        
+        void renderFrame(SceneNode* mRootNode, glm::vec4 debugShow, bool wireframe);
+        
+        void load();
+        void unload();
+    };
+public:
+    SharedResources* mSharedRes;
+    uint32_t mScreenWidth;
+    uint32_t mScreenHeight;
+
+
+    DeferredRenderer(uint32_t width, uint32_t height);
+    ~DeferredRenderer();
+    
+    void renderRootNode(SceneNode* rootNode);
+    
+    void load();
+    void unload();
+};
+
+}
+
+#endif // PGG_DEFERREDRENDERER_HPP
--- src/PepperGrains/DesignerGameLayer.hpp
+++ src/PepperGrains/DesignerGameLayer.hpp
@@ -32,92 +32,14 @@
 #include "TextModel.hpp"
 #include "TextureResource.hpp"
 #include "DebugInfiniteCheckerboardModel.hpp"
+#include "DeferredRenderer.hpp"
 
 namespace pgg {
 
 class DesignerGameLayer : public GameLayer {
 private:
-    struct GBuffer {
-        GLuint gFramebuffer;
-        
-        GLuint diffuseTexture;
-        GLuint normalTexture;
-        GLuint brightTexture;
-        GLuint depthStencilTexture;
-    };
-    GBuffer mGBuff;
 
-    struct ScreenShader {
-        ShaderProgramResource* shaderProg;
-        
-        GLuint diffuseHandle;
-        GLuint brightHandle;
-    };
-    ScreenShader mScreenShader;
-    
-    struct DebugScreenShader {
-        ShaderProgramResource* shaderProg;
-        
-        GLuint diffuseHandle;
-        GLuint normalHandle;
-        GLuint depthHandle;
-        GLuint brightHandle;
-        
-        GLuint viewHandle;
-    };
-    DebugScreenShader mDebugScreenShader;
-    
-    struct DebugSunShader {
-        ShaderProgramResource* shaderProg;
-        
-        GLuint mapHandles[PGG_NUM_SUN_CASCADES];
-        
-        GLuint viewHandle;
-    };
-    DebugSunShader mDebugSunShader;
-    
-    struct SkyStencilShader {
-        ShaderProgramResource* shaderProg;
-    };
-    SkyStencilShader mSkyStencilShader;
-    
-    struct DebugScreenFillShader {
-        ShaderProgramResource* shaderProg;
-        GLuint colorHandle;
-    };
-    DebugScreenFillShader mFillScreenShader;
-    
-    struct Camera {
-        float fov;
-        float aspect;
-        float nearDepth;
-        float farDepth;
-        
-        float cascadeBorders[PGG_NUM_SUN_CASCADES + 1];
-        
-        glm::mat4 viewMat;
-        glm::mat4 projMat;
-        glm::vec3 position;
-    };
-    Camera mCamera;
-    
-    struct Sky {
-        uint32_t sunTextureSize;
-        glm::vec3 sunDirection;
-        glm::vec3 sunPosition;
-        
-        // Todo: just swap out depth textures for each framebuffer
-        GLuint sunFramebuffers[PGG_NUM_SUN_CASCADES];
-        GLuint sunDepthTextures[PGG_NUM_SUN_CASCADES];
-        
-        glm::mat4 sunProjMatrices[PGG_NUM_SUN_CASCADES];
-        glm::mat4 sunViewMatrices[PGG_NUM_SUN_CASCADES];
-        
-        glm::mat4 sunBasicViewMatrix;
-        
-        SunLightModel* sunModel;
-    };
-    Sky mSky;
+    DeferredRenderer* mRenderer;
     
     nres::World* mEntityWorld;
     
@@ -147,11 +69,6 @@ private:
     uint32_t mScreenWidth;
     uint32_t mScreenHeight;
     
-    void loadGBuffer();
-    void unloadGBuffer();
-    void loadSun();
-    void unloadSun();
-    
     void renderFrame(glm::vec4 debugShow, bool wireframe);
 public:
     DesignerGameLayer(uint32_t width, uint32_t height);
@@ -163,10 +80,6 @@ public:
     SceneNode* mCamYawNode;
     SceneNode* mCamPitchNode;
     SceneNode* mCamRollNode;
-    
-    GLuint mFullscreenVao;
-    GLuint mFullscreenVbo;
-    GLuint mFullscreenIbo;
     
     FontResource* rainstormFont;
     TextModel* fpsCounter;
