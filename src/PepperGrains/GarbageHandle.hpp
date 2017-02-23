04f30926ddb5fbd5809035bda596edbb155e371f
Naftoreiclag <naftoreiclag@gmail.com>
2017-02-22 17:21:41 -0800

    Heavy Handle

--- src/PepperGrains/GarbageHandle.hpp
+++ src/PepperGrains/GarbageHandle.hpp
@@ -17,74 +17,201 @@
 #ifndef PGG_MEMORY_GARBAGEHANDLE_HPP
 #define PGG_MEMORY_GARBAGEHANDLE_HPP
 
+#include <functional>
 #include <stdint.h>
+
+// Debug only
+#include <iostream>
 
 namespace pgg {
 namespace Memory {
+
+class GarbageCollection;
     
-struct SharedGarbageData {
-    int32_t mNumCopies = 1;
-};
 
 class AbstractGarbageHandle {
-    virtual ~AbstractGarbageHandle();
-    virtual int32_t getNumCopies() = 0;
+public:
+    virtual ~AbstractGarbageHandle() { }
 };
 
 template <typename T> class GarbageHandle : public AbstractGarbageHandle {
+
+public:
+
+    /**
+     * Shared data among all copies of an instance. Contains the number of copies and also a pointer to the
+     * collection/factory which generated the object.
+     * 
+     * Also used to uniquely identify all copies of an instance.
+     */
+    struct Shared {
+        
+        /// The number of existing copies
+        uint32_t mNumCopies;
+        
+        /**
+         * The function used instead of "delete" if supplied.
+         * If empty, then "delete" is used. (Of course)
+         */
+        std::function<void(T*)> const mDeleteFunc;
+        
+        Shared(uint32_t numCopies = 0)
+        : mNumCopies(numCopies) {
+        }
+        
+        Shared(std::function<void(T*)> deleteFunc, uint32_t numCopies = 0)
+        : mDeleteFunc(deleteFunc)
+        , mNumCopies(numCopies) {
+        }
+    };
+    
 private:
     
-    SharedGarbageData* mShared;
+    /**
+     * Shared data among all copies of this instance.
+     */
+    Shared* mShared;
+    
+    /**
+     * The pointer to the underlying object. Can be accessed with -> operator or by calling getPtr().
+     * Arguably, storing the pointer here increases speed and decreases binary size at the cost of higher runtime
+     * memory usage.
+     */
     T* mWrapped;
     
-public:
-    /// Constructor
-    GarbageHandle(T* object)
-    : mShared(new SharedGarbageData())
-    , mWrapped(object) {
+    void incrementCopyCount() {
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tIncrement" << std::endl;
+        if(!mShared) return;
+        ++ mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tNum copies: " << mShared->mNumCopies << std::endl;
     }
     
-    /// Copy constructor
+    /**
+     * @brief Decreases the copy count by one. Calls owner cleanup method if copy counter reaches zero.
+     */
+    void decrementCopyCount() {
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tDecrement" << std::endl;
+        if(!mShared) return;
+        -- mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tNum copies: " << mShared->mNumCopies << std::endl;
+        if(mShared->mNumCopies == 0) {
+            if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tCalling cleanup..." << std::endl;
+            
+            if(mShared->mDeleteFunc) {
+                mShared->mDeleteFunc(mWrapped);
+            } else {
+                delete mWrapped;
+            }
+            
+            delete mShared;
+        }
+    }
+    
+public:
+    
+    /**
+     * @brief Default constructor, initializes to nullptr
+     */
+    GarbageHandle()
+    : mShared(nullptr)
+    , mWrapped(nullptr) {
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tEmpty handle" << std::endl;
+    }
+
+    /**
+     * @brief Used to construct the very first instance of this handle.
+     * 
+     * @param object The actual object being wrapped
+     */
+    GarbageHandle(T* object)
+    : mShared(new Shared())
+    , mWrapped(object) {
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tCreating new origin" << std::endl;
+        incrementCopyCount();
+    }
+    
+    /**
+     * @brief Used to construct the very first instance of this handle, also supplying deleter function
+     * 
+     * @param deleteFunc Memory where instance-shared data will be held
+     * @param object The actual object being wrapped
+     */
+    GarbageHandle(T* object, std::function<void(T*)> deleteFunc)
+    : mShared(new Shared(deleteFunc))
+    , mWrapped(object) {
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tCreating new origin" << std::endl;
+        incrementCopyCount();
+    }
+    
+    /// Copy constructor. Increments copy count.
     GarbageHandle(const GarbageHandle& rhs)
     : mShared(rhs.mShared)
     , mWrapped(rhs.mWrapped) {
-        ++ mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tCopy cstr" << std::endl;
+        incrementCopyCount();
     }
     
-    /// Move constructor
+    /// Move constructor. Increments copy count.
     GarbageHandle(GarbageHandle&& rhs)
     : mShared(rhs.mShared)
     , mWrapped(rhs.mWrapped) {
-        ++ mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tMove cstr" << std::endl;
+        incrementCopyCount();
+    }
+    
+    /**
+     * @brief Equivalent to assignment
+     * @param rhs The object to replace this one
+     */
+    void replace(const GarbageHandle& rhs) {
+        if(rhs.mShared == mShared) return;
+        decrementCopyCount();
+        mShared = rhs.mShared;
+        mWrapped = rhs.mWrapped;
+        incrementCopyCount();
+    }
+    
+    /**
+     * @brief Releases the resource. Analogous to setting a pointer to null
+     */
+    void reset() {
+        decrementCopyCount();
+        mShared = nullptr;
+        mWrapped = nullptr;
+    }
+    
+    void isNull() {
+        return mShared == nullptr;
     }
     
     /// Copy assignment operator
     GarbageHandle& operator=(const GarbageHandle& rhs) {
-        -- mShared->mNumCopies;
-        mShared = rhs.mShared;
-        mWrapped = rhs.mWrapped;
-        ++ mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tCopy assn" << std::endl;
+        replace(rhs);
+        
+        return *this;
     }
     
     /// Move assignment operator
     GarbageHandle& operator=(GarbageHandle&& rhs) {
-        -- mShared->mNumCopies;
-        mShared = rhs.mShared;
-        mWrapped = rhs.mWrapped;
-        ++ mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tMove assn" << std::endl;
+        replace(rhs);
+        
+        return *this;
     }
     
     /// Destructor
     ~GarbageHandle() {
-        -- mShared->mNumCopies;
+        if(mShared) std::cout << mShared; else std::cout << "0x_______"; std::cout << "\t" << this << "\tDstr" << std::endl;
+        decrementCopyCount();
     }
     
     T* operator->() {
         return mWrapped;
     }
     
-    int32_t getNumCopies() {
-        return mShared->mNumCopies;
+    T* getPtr() {
+        return mWrapped;
     }
 };
 
